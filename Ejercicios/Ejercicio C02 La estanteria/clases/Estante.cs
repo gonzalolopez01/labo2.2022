2d8d037aa3704cdb83fce2acb09a9323656b0197
gonzalolopez01 <gonzz777@gmail.com>
2022-09-06 14:34:31 -0300

    ej c02

---
+++ Ejercicios/Ejercicio C02 La estanteria/clases/Estante.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C02_La_estanteria.clases
+{
+    internal class Estante
+    {
+        private Producto[] _productos;
+        private int _ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            _ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos {
+            get { return _productos; }
+        }
+        
+        public static string MostrarEstante(Estante e)
+        {
+            string ret;
+            StringBuilder sb = new StringBuilder();
+            int len = e._productos.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (e._productos[i] is not null) //se usa is not null porque el operador == esta sobrecargado
+                {
+                    sb.AppendLine(Producto.MostrarProducto(e._productos[i]));
+                }
+            }
+            return ret = sb.ToString();
+        }
+        public static bool operator == (Estante e, Producto p)
+        {
+            bool ret = false;
+            int len = e._productos.Length;
+            for(int i = 0; i < len; i++)
+            {
+                if (e._productos[i] is not null)
+                {
+                    if (e._productos[i] == p)
+                    {
+                        ret = true;
+                    }
+                    break;
+                }
+            }
+            return ret;
+        }
+        public static bool operator != (Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+        public static bool operator + (Estante e, Producto p)
+        {
+            bool ret = false;
+            int len;
+            if(e != p)
+            {
+                len = e._productos.Length;
+                for(int i = 0; i < len; i++)
+                {
+                    if(e._productos[i] is null)
+                    {
+                        e._productos[i] = p;
+                        ret = true;
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if(e == p)
+            {
+                int len = e._productos.Length;
+                for(int i = 0; i < len; i++)
+                {
+                    if (e._productos[i] == p)
+                    {
+                        e._productos[i] = null;
+                        break;
+                    }
+                }
+            }
+
+            return e;
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio C02 La estanteria/clases/Producto.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_C02_La_estanteria.clases
+{
+    internal class Producto
+    {
+        private string _codigoDeBarras;
+        private string _marca;
+        private float _precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            _codigoDeBarras = codigo;
+            _marca = marca;
+            _precio = precio;
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p._codigoDeBarras;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(p._marca + "-");
+            sb.Append(p._codigoDeBarras + "-");
+            sb.AppendLine(p._precio.ToString());
+
+            return sb.ToString();
+        }
+        public string GetMarca{
+            get { return _marca; }
+            set { _marca = value; } 
+        }
+        public float GetPrecio
+        {
+            get { return _precio; }
+            set { _precio = value; }
+        }
+        public static bool operator ==(Producto p, string cadena)
+        {
+            bool ret = false;
+            if(p is not null)
+            {
+                ret = p._marca == cadena;
+            }
+            return ret;
+            //return p._marca == cadena;
+        }
+        public static bool operator !=(Producto p, string cadena)
+        {
+            return !(p._marca == cadena);
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool ret = false;
+            if(p1 is not null && p2 is not null)
+            {
+                ret = (p1._marca == p2._marca && p1._codigoDeBarras == p2._codigoDeBarras);
+                //return p1 == p2._marca && p1._codigoDeBarras == p2._codigoDeBarras;
+            }
+            return ret;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+       /* public static bool operator ==(Producto p, string marca)
+        {
+            bool ret = false;
+            if(p is not null)
+            {
+                ret = (p._marca == marca);
+            }
+            return ret;
+        }
+        public static bool operator != (Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool ret = false;
+            if(p1 is not null && p2 is not null)
+            {
+                ret = (p1._marca == p2._marca && p1._codigoDeBarras == p2._codigoDeBarras);
+            }
+            return ret;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }*/
+    }
+}
